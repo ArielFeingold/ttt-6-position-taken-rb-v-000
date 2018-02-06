3262cc48e7d91b6cc3675181d5c5cff6373098e1
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-06 18:21:23 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   end
-  if board[index] == X || O
+else
     return true
   end
 end
